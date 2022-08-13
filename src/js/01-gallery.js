25288cf7b8e33305d3900a69498eeddd9263b9b5
Albina Bondar <albinabondar2019@gmail.com>
2022-08-14 01:00:46 +0300

    add 1 exersize

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,23 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery")
+
+gallery.innerHTML = galleryItems.map(({ preview, original, description }) => 
+  `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>  `
+).join("")
+
+const lightBox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom',
+})
