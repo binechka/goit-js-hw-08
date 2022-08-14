d75b4dce5c6c71bf211421b9fee31e20b60d3d12
Albina Bondar <albinabondar2019@gmail.com>
2022-08-14 16:05:51 +0300

    addd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('#vimeo-player');
+    const player = new Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+player.on('timeupdate', function (data) {
+     data = {
+ duration: 61.857,
+    percent: 0.049,
+    seconds: 3.034
+     }
+console.log(data);
+localStorage.setItem("videoplayer-current-time",data.seconds);
+data.seconds = iframe.currentTime
+     // data is an object containing properties specific to that event
+});
+
+// let localStorage=localStorage.setItem("videoplayer-current-time", data.duration);
+// console.log(localStorage);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".form")
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+  const {
+    elements: { email, message }
+  } = event.currentTarget;
+    const setedItems = localStorage.setItem("feedback-form-state", event.currentTarget)
+    if (setedItems) {
+        console.log(setedItems);
+    }
+
+    let obj = { email: email.value, message: message.value };
+    console.log(obj);
+    event.currentTarget.reset();
+  
+});
 
