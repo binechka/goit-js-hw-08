9676cabe430689320364e68c573cdb1268a6ff08
binechka <albinabondar2019@gmail.com>
2022-08-15 17:36:10 +0000

    Deploying to gh-pages from @ binechka/goit-js-hw-08@7af358d932a6dfe9866a0ad47945065b5c760f83 🚀

--- 03-feedback.fa79afd7.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault();var t=e.currentTarget.elements,r=t.email,a=t.message,l=localStorage.setItem("feedback-form-state",e.currentTarget);l&&console.log(l);var o={email:r.value,message:a.value};console.log(o),e.currentTarget.reset()}));
-//# sourceMappingURL=03-feedback.fa79afd7.js.map
